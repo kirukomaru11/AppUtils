e30810eda32989af13187a343a763d49d4e85bf1
kirukomaru11 <kiruko@duck.com>
2025-11-25 03:42:59 -0300

    AppInstall.sh: revert to /usr/lib, use install for desktop, fix AppUtils

--- AppInstall.sh
+++ AppInstall.sh
@@ -5,11 +5,12 @@ BINARY=$(xmllint metainfo.xml --noout --xpath "//binary/text()")
 COMMENT=$(xmllint metainfo.xml --noout --xpath "//summary/text()")
 CATEGORIES=$(xmllint metainfo.xml --noout --xpath "//category/text()" | paste -sd ";" -)
 KEYWORDS=$(xmllint metainfo.xml --noout --xpath "//keyword/text()" | paste -sd ";" -)
-PYTHON=$(ls /app/lib/ | grep python)
-install -D .AppUtils/AppUtils.py /app/lib/$PYTHON/site-packages/AppUtils.py
+PYTHON=$(ls /usr/lib/ | grep python)
+install -D AppUtils/AppUtils.py /app/lib/$PYTHON/site-packages/AppUtils.py
 install -Dm755 main.py /app/bin/$BINARY
 install -D metainfo.xml /app/share/metainfo/$ID.metainfo.xml
-echo -e "[Desktop Entry]\nType=Application\nName=$NAME\nComment=$COMMENT\nIcon=$ID\nExec=$BINARY\nCategories=$CATEGORIES\nKeywords=$KEYWORDS" > /app/share/applications/$ID.desktop
+echo -e "[Desktop Entry]\nType=Application\nName=$NAME\nComment=$COMMENT\nIcon=$ID\nExec=$BINARY\nCategories=$CATEGORIES\nKeywords=$KEYWORDS" > $BINARY.desktop
+install -D $BINARY.desktop /app/share/applications/$ID.desktop
 install -D app.svg /app/share/icons/hicolor/scalable/apps/$ID.svg
 install -D app-symbolic.svg /app/share/icons/hicolor/symbolic/apps/$ID-symbolic.svg
 for file in *.svg; do
