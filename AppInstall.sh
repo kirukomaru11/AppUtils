800589d6d63e166b2d33c986e0be914c38cede41
kirukomaru11 <kiruko@duck.com>
2025-12-28 03:01:14 -0300

    AppInstall.sh: support MimeType and opening files

--- AppInstall.sh
+++ AppInstall.sh
@@ -6,10 +6,15 @@ COMMENT=$(xmllint metainfo.xml --noout --xpath "//summary/text()")
 CATEGORIES=$(xmllint metainfo.xml --noout --xpath "//category/text()" | paste -sd ";" -)
 KEYWORDS=$(xmllint metainfo.xml --noout --xpath "//keyword/text()" | paste -sd ";" -)
 PYTHON=$(ls /usr/lib/ | grep python)
+DESKTOP="[Desktop Entry]\nType=Application\nName=$NAME\nComment=$COMMENT\nIcon=$ID\nExec=$BINARY"
+if [[ "$(xmllint metainfo.xml --noout --xpath "//mediatype/text()")" != "" ]]; then
+    DESKTOP+=" %U\nMimeType=$(xmllint metainfo.xml --noout --xpath '//mediatype/text()' | paste -sd ';' -)"
+fi
+DESKTOP+="\nCategories=$CATEGORIES\nKeywords=$KEYWORDS"
+echo -e $DESKTOP > $BINARY.desktop
 install -D AppUtils/AppUtils.py /app/lib/$PYTHON/site-packages/AppUtils.py
 install -Dm755 main.py /app/bin/$BINARY
 install -D metainfo.xml /app/share/metainfo/$ID.metainfo.xml
-echo -e "[Desktop Entry]\nType=Application\nName=$NAME\nComment=$COMMENT\nIcon=$ID\nExec=$BINARY\nCategories=$CATEGORIES\nKeywords=$KEYWORDS" > $BINARY.desktop
 install -D $BINARY.desktop /app/share/applications/$ID.desktop
 install -D app.svg /app/share/icons/hicolor/scalable/apps/$ID.svg
 install -D app-symbolic.svg /app/share/icons/hicolor/symbolic/apps/$ID-symbolic.svg
